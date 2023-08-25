a002dfc3bce26fdac2e8b3b4e1454b0ae6ba1e9a
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-25 22:14:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23830477'))
 API_HASH = environ.get('API_HASH', '19f8365d98fb11c9cd6c1eaa8b1fa4b8')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs')
-PORT = environ.get("PORT", "3000")
+PORT = environ.get("PORT", "9090")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
